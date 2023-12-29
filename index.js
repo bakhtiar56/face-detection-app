9406b10f454ca828d1b624969cba7e03bad580df
Bakhtiar Rasheed <bakhtiar.baloch55@hotmail.com>
2023-12-29 11:11:27 +0500

    update code

--- index.js
+++ index.js
@@ -1,67 +1,81 @@
 import {
-    FaceDetector,
-    FilesetResolver,
-  } from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0";
+  FaceDetector,
+  FilesetResolver,
+} from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0";
+
+let total_times_clicked = 0
+
+const button = document.getElementById("button")
+
+button.onclick = loadResult
+
+const initializefaceDetector = async () => {
+
+  const vision = await FilesetResolver.forVisionTasks(
+    "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm"
+  );
+
+  let faceDetector = await FaceDetector.createFromOptions(vision, {
+    baseOptions: {
+      modelAssetPath: `https://storage.googleapis.com/mediapipe-models/face_detector/blaze_face_short_range/float16/1/blaze_face_short_range.tflite`,
+      delegate: "GPU"
+    },
+  });
+
+  const image = document.getElementById("image")
+
+  const faceDetectorResult = faceDetector.detect(image).detections;
+
+  return faceDetectorResult
+};
+
+export async function loadResult() {
+
+  total_times_clicked = total_times_clicked + 1
+
+  if (total_times_clicked == 1) {
+
+    button.disabled = "true"
+  }
 
 
-  const initializefaceDetector = async () => {
-    const vision = await FilesetResolver.forVisionTasks(
-      "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm"
-    );
-    let faceDetector = await FaceDetector.createFromOptions(vision, {
-      baseOptions: {
-        modelAssetPath: `https://storage.googleapis.com/mediapipe-models/face_detector/blaze_face_short_range/float16/1/blaze_face_short_range.tflite`,
-        delegate: "GPU"
-      },
-    });
+  initializefaceDetector().then((res) => {
     const image = document.getElementById("image")
-  
-            console.log(image)
-            const faceDetectorResult = faceDetector.detect(image).detections;
-            console.log(faceDetectorResult)
-            return faceDetectorResult
-  };
 
-  async function  load(){
- 
-
-  
-
-
-  initializefaceDetector().then((res)=> {
-    const image = document.getElementById("image")
- 
-    const canvas=document.createElement("canvas")
+    const canvas = document.createElement("canvas")
     document.body.appendChild(canvas);
 
-    canvas.width=image.width
-    canvas.height=image.height
-    const ctx=canvas.getContext("2d")
+    canvas.width = image.width
+    canvas.height = image.height
+    const ctx = canvas.getContext("2d")
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    ctx.drawImage(image,0,0)
+
+    // Draw new image
+    ctx.drawImage(image, 0, 0)
     ctx.strokeStyle = 'red';
-    ctx.font = "bold 30px arial";
+    ctx.font = "bold 20px arial";
+    ctx.fillStyle = "green";
+
+    // Draw text
 
     ctx.beginPath();
-    ctx.fillText("Confidence Level:",res[0].boundingBox.originX-150 , res[0].boundingBox.originY-50,180);
-    ctx.fillText(""+Math.round(res[0].categories[0].score*100)/100,res[0].boundingBox.originX+40, res[0].boundingBox.originY-50,180);
+    ctx.fillText("Confidence Level:", res[0].boundingBox.originX - 90, res[0].boundingBox.originY - 60, 180);
 
+    ctx.fillText("" + Math.round(res[0].categories[0].score * 100) / 100, res[0].boundingBox.originX + 90, res[0].boundingBox.originY - 60, 180);
 
-    console.log(res[0].boundingBox.originX)
-    ctx.lineWidth=2;
-  ctx.rect(res[0].boundingBox.originX, res[0].boundingBox.originY, res[0].boundingBox.width, res[0].boundingBox.height)
+    // Draw bounding box
+
+    ctx.lineWidth = 2;
+    ctx.rect(res[0].boundingBox.originX, res[0].boundingBox.originY, res[0].boundingBox.width, res[0].boundingBox.height)
     ctx.stroke()
 
 
 
   })
 
+}
 
 
-
-
-  }
-  load()
 
 
